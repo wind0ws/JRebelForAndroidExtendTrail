63ab60db52518457cc861c70a856c9363fe6e8ab
wind0ws <932803995@qq.com>
2016-12-01 11:55:31 +0800

    添加项目文件。

---
+++ JRebelForAndroidExtendTrail/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace JRebelForAndroidExtendTrail
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==============   Welcome  =============\r\n");
+
+            string todayDate = DateTime.Now.ToString("yyyy-MM-dd");
+            RegistryKey currentUserRegistery = Registry.CurrentUser;
+            string subKey = "SOFTWARE\\JavaSoft\\Prefs\\jrebel-android";
+            string startTrailTime = RegistryHelper.GetRegistryData(currentUserRegistery, subKey, "tt");
+            string endTrailTime = RegistryHelper.GetRegistryData(currentUserRegistery, subKey, "te");
+           
+            Console.WriteLine("Current start trail time is: "+startTrailTime);
+            Console.WriteLine("Current  end  trail time is: " + endTrailTime);
+
+            if (todayDate != startTrailTime.Substring(0, 10))
+            {
+                startTrailTime = todayDate + startTrailTime.Substring(10);
+                Console.WriteLine("Set trail start time to: " + startTrailTime);
+                RegistryHelper.SetRegistryData(currentUserRegistery, subKey, "tt", startTrailTime);
+            }
+            int currentYear = DateTime.Now.Year;
+            int trailEndYear = int.Parse(endTrailTime.Substring(0, 4));
+            if (trailEndYear <= currentYear)
+            {
+                endTrailTime = (currentYear + 10).ToString() + endTrailTime.Substring(4);
+                Console.WriteLine("Set trail end  time to: "+ endTrailTime);
+                RegistryHelper.SetRegistryData(currentUserRegistery, subKey, "te", endTrailTime);
+            }
+
+            Console.WriteLine("\r\nOk,Done.Press any key to exit...");
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ JRebelForAndroidExtendTrail/RegistryHelper.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JRebelForAndroidExtendTrail
+{
+    class RegistryHelper
+    {
+        /// <summary>
+        /// 读取指定名称的注册表的值
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static string GetRegistryData(RegistryKey root, string subkey, string name)
+        {
+            string registData = string.Empty;
+            RegistryKey myKey = root.OpenSubKey(subkey, true);
+            if (myKey != null)
+            {
+                registData = myKey.GetValue(name).ToString();
+            }
+            return registData;
+        }
+
+        /// <summary>
+        /// 向注册表中写数据
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="tovalue"></param> 
+        public static void SetRegistryData(RegistryKey root, string subkey, string name, string value)
+        {
+            RegistryKey aimdir = root.CreateSubKey(subkey);
+            aimdir.SetValue(name, value);
+        }
+
+        /// <summary>
+        /// 删除注册表中指定的注册表项
+        /// </summary>
+        /// <param name="name"></param>
+        public static void DeleteRegistry(RegistryKey root, string subkey, string name)
+        {
+            string[] subkeyNames;
+            RegistryKey myKey = root.OpenSubKey(subkey, true);
+            subkeyNames = myKey.GetSubKeyNames();
+            foreach (string aimKey in subkeyNames)
+            {
+                if (aimKey == name)
+                    myKey.DeleteSubKeyTree(name);
+            }
+        }
+
+        /// <summary>
+        /// 判断指定注册表项是否存在
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static bool IsRegistryExist(RegistryKey root, string subkey, string name)
+        {
+            bool _exit = false;
+            string[] subkeyNames;
+            RegistryKey myKey = root.OpenSubKey(subkey, true);
+            subkeyNames = myKey.GetSubKeyNames();
+            foreach (string keyName in subkeyNames)
+            {
+                if (keyName == name)
+                {
+                    _exit = true;
+                    return _exit;
+                }
+            }
+            return _exit;
+        }
+
+    }
+}
